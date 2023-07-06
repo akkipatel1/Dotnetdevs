2d60b0a53b920a122c70c8b733d8abed46ca2e91
rahulchaudhary2001 <138780631+rahulchaudhary2001@users.noreply.github.com>
2023-07-06 18:40:12 +0530

    Add files via upload

---
+++ AbstractandInteface/AbstractandInterface/AbstractandInterface/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+public abstract class abstractClass
+{
+
+    public abstract void abstractMethod();
+}
+
+interface INteface1
+{
+    void show();
+}
+
+public class MyClass : abstractClass, INteface1
+{
+    public void show()
+    {
+        Console.WriteLine("Hello");
+    }
+    public override void abstractMethod()
+    {
+        Console.WriteLine("This is method");
+    }
+
+    public static void Main(string[] args)
+    {
+        MyClass obj= new MyClass(); 
+        obj.show();
+        obj.abstractMethod();
+    }
+}
